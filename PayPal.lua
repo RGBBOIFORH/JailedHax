f84ed17cf52540ea1dd33def82f8e5727f6e1774
HazeWasTaken <56366650+HazeWasTaken@users.noreply.github.com>
2020-08-08 22:38:02 +0200

    Update PayPal.lua

--- PayPal.lua
+++ PayPal.lua
@@ -1,5 +1,5 @@
---if PROTOSMASHER_LOADED or secure_load and "Sentinel" or is_sirhurt_closure and " Sirhurt" or is_synapse_function and "SynapseX" then
+if PROTOSMASHER_LOADED or secure_load and "Sentinel" or is_sirhurt_closure and " Sirhurt" or is_synapse_function and "SynapseX" then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/HazeWasTaken/JailedHax/master/Paid", true))()
---else
---loadstring(game:HttpGet("https://raw.githubusercontent.com/HazeWasTaken/JailedHax/master/SonaorFree", true))()
---end
+else
+loadstring(game:HttpGet("https://raw.githubusercontent.com/HazeWasTaken/JailedHax/master/SonaorFree", true))()
+end
